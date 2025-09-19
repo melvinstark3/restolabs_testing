ba33d3ca016c31abd35ff35661d6cc995b91883d
Kartik <kartik@restolabs.com>
2025-09-19 18:57:01 +0530

    Apply Try & Catch to Verify Loyalty Application

--- src/main/java/org/example/applyRestoLoyalty.java
+++ src/main/java/org/example/applyRestoLoyalty.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
@@ -11,7 +13,12 @@ public class applyRestoLoyalty extends browserSetup{
         loyaltyInputField.clear();
         loyaltyInputField.sendKeys(readProperty("loyaltyValue"));
         driver.findElement(By.xpath("//button[@data-testid=\"openLoyaltyPopUp\"]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class=\"tip__remove text-xs font-semibold text-red-600 bg-red-100 py-0.5 px-2 hover:bg-app-gray-100 transition-all duration-300 rounded-full ng-star-inserted\"]")));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h6[contains(@data-testid, 'Loyalty Points')]")));
+        try{
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class=\"tip__remove text-xs font-semibold text-red-600 bg-red-100 py-0.5 px-2 hover:bg-app-gray-100 transition-all duration-300 rounded-full ng-star-inserted\"]")));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h6[contains(@data-testid, 'Loyalty Points')]")));
+            System.out.println("CASE 31: FAIL: Loyalty Points has been applied for the Order");
+        } catch (NoSuchElementException | TimeoutException e){
+            System.out.println("CASE 31: FAIL: Loyalty Points application was Unsuccessful");
+        }
     }
 }
