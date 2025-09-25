357dbc51d013f2fa119014a7c74482ee8ba6f7c8
Kartik <kartik@restolabs.com>
2025-09-25 15:50:13 +0530

    Fix Como Login Button Xpath

--- src/main/java/org/example/loginOrder.java
+++ src/main/java/org/example/loginOrder.java
@@ -23,9 +23,9 @@ public class loginOrder extends browserSetup{
             driver.findElement(By.id("guest_user")).click();
             driver.findElement(By.xpath("//button[normalize-space()='Get Code by SMS']")).click();
             driver.findElement(By.id("phone")).sendKeys(readProperty("comoLoginNumber"));
-            driver.findElement(By.xpath("//span[@class='ng-tns-c1779389301-0']")).click();
+            driver.findElement(By.xpath("//span[@class=\"ng-tns-c1779389301-1\"]")).click();
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("otp1"))).sendKeys(new getComoOTP().getOTP());
-            driver.findElement(By.xpath("//span[@class='ng-tns-c1779389301-0']")).click();
+            driver.findElement(By.xpath("//span[@class=\"ng-tns-c1779389301-1\"]")).click();
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class=\"inline-flex flex-shrink-0 justify-center items-center h-6 w-6 rounded-md text-gray-500 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-sm dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800\"]"))).click();
         }
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@aria-label=\""+ orderMode +"\"]")));
