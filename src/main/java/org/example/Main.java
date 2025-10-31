8f53537b7eed70b3b202013e50c8a1ce7431bdd4
Kartik <kartik@restolabs.com>
2025-10-31 18:47:19 +0530

    Only Use COD to Place QMS Orders

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ public class Main extends browserSetup{
     public static void main(String[] args) throws InterruptedException {
         invokeBrowser();
         new codOrder();
-        new pickupOrder();
-        new dineInOrder();
-        new homeDeliveryOrder();
+        quitBrowser();
     }
 }
--- src/main/java/org/example/codOrder.java
+++ src/main/java/org/example/codOrder.java
@@ -2,11 +2,8 @@ package org.example;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.sql.Time;
-import java.util.List;
+import java.util.ArrayList;
 
 public class codOrder extends browserSetup{
     public codOrder() throws InterruptedException {
@@ -15,56 +12,33 @@ public class codOrder extends browserSetup{
         wait = new WebDriverWait(driver, 30);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@aria-label=\"Pick Up\"]")));
         driver.findElement(By.xpath("//button[@aria-label=\"Pick Up\"]")).click();
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        String Location = readProperty("GuestLocation");
-        new createCart("Pick Up",Location,loggedIn, readProperty("codOrderTime"),readProperty("preOrderTime"));
-        wait = new WebDriverWait(driver, 30);
-        System.out.println("Entering Customer Details");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@data-testid=\"first_name\"]")));
-        driver.findElement(By.xpath("//input[@data-testid=\"first_name\"]")).sendKeys(readProperty("GuestFirstName"));
-        driver.findElement(By.xpath("//input[@data-testid=\"last_name\"]")).sendKeys(readProperty("GuestLastName"));
-        driver.findElement(By.xpath("//input[@data-testid=\"phone\"]")).sendKeys(readProperty("GuestPhoneNumber"));
-        driver.findElement(By.name("email")).sendKeys(readProperty("GuestEmail"));
-        driver.findElement(By.xpath("//button[@class=\"primary_button w-full text-base font-semibold p-3 px-5 mr-3 rounded-2xl border capitalize text-white ng-star-inserted\"]")).click();
-        // Stale Element Exception if Trying Implicit Wait
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("\"//button[@class=\\\"primary_button w-full text-base font-semibold p-3 px-5 mr-3 rounded-2xl border capitalize text-white ng-star-inserted\\\"]\"")));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[@data-testid=\"orderTotal\"]")));
-        js.executeScript("window.scrollBy(0,2000)", "");
-        System.out.println("CASE 3: For Guest Order: ");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-testid=\"continue_order\"]")));
-        Thread.sleep(2000);
-        // The System automatically selects back the PaymentMode0 regardless of click, We can remove sleep once dev team fixes this
-        try{
-            wait = new WebDriverWait(driver, 3);
-            if (driver.findElement(By.id("policy")).isSelected()) {
-                System.out.println("Privacy Policy and Terms & Conditions are Already Accepted");
-            } else {
-                driver.findElement(By.id("policy")).click();
-                System.out.println("Privacy Policy and Terms & Conditions Accepted");
+        ArrayList<String> itemList = new ArrayList<String>();
+        itemList.add("Customizable Item");
+        itemList.add("Customizable Nested Item");
+        itemList.add("Normal Item");
+        itemList.add("Item with Minimum Quantity");
+        itemList.add("Item with Maximum Quantity");
+        itemList.add("Item & Location Tax Item");
+        itemList.add("Location Tax Excluded Item");
+        itemList.add("Only Item Tax Item");
+        itemList.add("2-6PM Weekdays Item");
+        itemList.add("Time Category Normal Item");
+
+        for(String item:itemList){
+            new createCart("Pick Up",readProperty("GuestLocation"),loggedIn, readProperty("codOrderTime"),readProperty("preOrderTime"), item);
+            new checkout();
+            try {
+                wait = new WebDriverWait(driver, 60);
+                String restartOrderButtonXpath = "//div[@class='bg-white rounded-xl border border-app-gray-300']//span[@class='border-dashed text-sm font-semibold border px-2 py-0.5 rounded-lg cursor-pointer ml-2'][normalize-space()='Click here to start order again']";
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(restartOrderButtonXpath)));
+                System.out.println("CASE 3: PASS: Guest Order was Successful");
+                System.out.println("CASE 10: PASS: Place an Order with " + readProperty("codOrderTime") + " Timing");
+                System.out.println("CASE 12: PASS: Cash on Delivery Payment was Successful");
+            } catch (NoSuchElementException | TimeoutException e){
+                System.out.println("CASE 3: FAIL: Guest Order wasn't Posted in Time. Please Check the Case Manually");
+                System.out.println("CASE 10: FAIL: " + readProperty("codOrderTime") + " Order wasn't Posted in Time");
+                System.out.println("CASE 12: FAIL: Cash on Delivery Order wasn't Posted in Time");
             }
         }
-        catch (NoSuchElementException | TimeoutException e) {
-            System.out.println("Privacy Policy and Terms and Conditions Checkbox is Not Displayed");
-        }
-        wait = new WebDriverWait(driver, 30);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[@data-testid=\"orderTotal\"]")));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-testid=\"continue_order\"]")));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@data-testid=\""+readProperty("CODPaymentMode")+"\"]")));
-        driver.findElement(By.xpath("//input[@data-testid=\""+readProperty("CODPaymentMode")+"\"]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-testid=\"continue_order\"]")));
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@data-testid=\"continue_order\"]"))).click();
-        try {
-            wait = new WebDriverWait(driver, 60);
-            String restartOrderButtonXpath = "//div[@class='bg-white rounded-xl border border-app-gray-300']//span[@class='border-dashed text-sm font-semibold border px-2 py-0.5 rounded-lg cursor-pointer ml-2'][normalize-space()='Click here to start order again']";
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(restartOrderButtonXpath)));
-            System.out.println("CASE 3: PASS: Guest Order was Successful");
-            System.out.println("CASE 10: PASS: Place an Order with " + readProperty("codOrderTime") + " Timing");
-            System.out.println("CASE 12: PASS: Cash on Delivery Payment was Successful");
-        } catch (NoSuchElementException | TimeoutException e){
-            System.out.println("CASE 3: FAIL: Guest Order wasn't Posted in Time. Please Check the Case Manually");
-            System.out.println("CASE 10: FAIL: " + readProperty("codOrderTime") + " Order wasn't Posted in Time");
-            System.out.println("CASE 12: FAIL: Cash on Delivery Order wasn't Posted in Time");
-        }
-
     }
 }
