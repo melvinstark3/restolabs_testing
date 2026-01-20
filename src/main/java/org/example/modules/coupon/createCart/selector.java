037b0f6ccb149a5b5fe7781fbc34d0c115bc0501
Kartik <kartik@restolabs.com>
2026-01-20 15:01:55 +0530

    Coupon: Selector: Wait for Location to be Clickable

--- src/main/java/org/example/modules/coupon/createCart/selector.java
+++ src/main/java/org/example/modules/coupon/createCart/selector.java
@@ -44,7 +44,8 @@ public class selector extends browserSetup {
                 }
             }
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[@data-testid=\"deliveryTitle\"]")));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[normalize-space()='" + Location + "']"))).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[normalize-space()='" + Location + "']")));
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//h5[normalize-space()='" + Location + "']"))).click();
         }
 //        try{
 //            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[@data-testid=\"deliveryTitle\"]")));
@@ -70,7 +71,8 @@ public class selector extends browserSetup {
                 System.out.println("Selecting Pickup Location from the List");
             }
         }
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[normalize-space()='" + Location + "']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[normalize-space()='" + Location + "']")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//h5[normalize-space()='" + Location + "']"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h5[@data-testid=\"deliveryTitle\"]")));
         js.executeScript("window.scrollBy(0,2000)", "");
         try {
